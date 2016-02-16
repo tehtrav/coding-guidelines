02268619a37f522cd2c9ebc80d6e38ace63785c2
Travis Lehman <travlehman@gmail.com>
2016-02-16 16:10:31 -0500

    Styled hero and nav

--- index.php
+++ index.php
@@ -12,29 +12,41 @@
 
 </head>
 <body>
-	<header class="global-header">
-
-	</header>
-
-
+	<div id="hero-mast" class="jumbotron">
+        <div class="container">
+            <h1>Coding Guidlines</h1>
+            <p>Some best-practice rules to follow when writing Front-End code for internal &amp; external Clark projects.</p>
+        </div>
+    </div>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
-
+                <ul class="documentation-nav nav nav-stacked">
+                    <li><a href="#">Syntax formatting and spacing</a></li>
+                    <li><a href="#">Selector grouping</a></li>
+                    <li><a href="#">Property order</a></li>
+                    <li><a href="#">Quotes</a></li>
+                    <li><a href="#">Numbers</a></li>
+                    <li><a href="#">Units</a></li>
+                    <li><a href="#">Color values</a></li>
+                    <li><a href="#">Writing SCSS</a></li>
+                    <li><a href="#">Nesting selectors</a></li>
+                    <li><a href="#">Extend rules</a></li>
+                    <li><a href="#">Selector specificity</a></li>
+                    <li><a href="#">Avoid over-specifying</a></li>
+                    <li><a href="#">HTML Guidelines</a></li>
+                    <li><a href="#">General formatting</a></li>
+                    <li><a href="#">Boolean attributes</a></li>
+                </ul>
 			</div>
 			<div class="col-md-9">
-
-
-
                 <section class="page-section">
-
                     <h1 class="page-header">SCSS/CSS Guidelines</h1>
-
                     <h2>Syntax formatting and spacing</h2>
                     <div class="content-chunk">
                         <p>To make sure we're all writing similarly-readable code we want to try to format our as so:</p>
                         <ul>
-                            <li>Use four space indents ("soft tabs") rather than tabs ("hard tabs")</li>
+                            <li>Use four space indents (&ldquo;soft tabs&rdquo;) rather than tabs (&ldquo;hard tabs&rdquo;)</li>
                             <li>Put spaces after <code>:</code> in property declarations.</li>
                             <li>Put spaces before <code>{</code> in rule declarations.</li>
                             <li>Put an empty line bewtween rule declarations.</li>
@@ -99,18 +111,13 @@
 
                     <div class="content-chunk">
                         <h3>Quotes</h3>
-
                         <p>When possible use double quotes to wrap strings</p>
                         <pre><code>background-image: url("/images/kittens.jpg");</code></pre>
-
-                        <p></p>
                     </div>
 
                     <div class="content-chunk">
                         <h3>Numbers</h3>
-
                         <p>Avoid using unnessesary zeros.</p>
-
                         <pre><code>// Yes
 .thing {
     padding: 1rem;
@@ -128,39 +135,29 @@
 
                     <div class="content-chunk">
                         <h3>Units</h3>
-
                         <p>0 value should never ever have a unit.</p>
-
                         <pre><code>// Yes
 margin: 0;
 
 // No
 margin: 0px;</code></pre>
-
                         </div>
-
                         <div class="content-chunk">
                             <h3>Color values</h3>
-
                             <ul>
                                 <li>Avoid using CSS color nicknames like <code>blue</code> and <code>red</code>.</li>
                                 <li>Use hex color codes like <code>#fff</code>, or <code>rgba()</code>.</li>
                                 <li>Utilize Sass' <code>rgba(#000, .5)</code> function for adding alpha to a hex color code.</li>
                                 <li>Avoid naming global variables after their appearance (good: <code>$brand-primary-color</code>, bad: <code>$blue</code>).</li>
                             </ul>
-
-
                             <pre><code>.thing {
     color: #333;
     border: 1px solid rgba(#000, .2);
 }</code></pre>
-
                         </div>
-
 
                         <h2>Writing SCSS</h2>
                         <p>Many of the amazingly powerful features of Sass come with equally significant downfalls if used wrecklessly. Be aware of those things and plan accordingly.</p>
-
                         <div class="content-chunk">
                             <h3>Nesting selectors</h3>
                             <p>Selector nesting in Sass is a feature that's often used based on convenience rather than intention. It's easy to create a selector explosion by nesting as much as possible without thought as to what CSS is required to perform the same thing.</p>
@@ -175,7 +172,6 @@ margin: 0px;</code></pre>
     }
 }</code></pre>
                             <p>If you're feeling constrained by the 3 level rule, consider using more descriptive class names.</p>
-
                             <pre><code>// Bad
 .product {
     ...
@@ -201,25 +197,20 @@ margin: 0px;</code></pre>
 }</code></pre>
 
                                 <h3>Extend rules</h3>
-
                                 <p><code>@extend</code> rules are one of the most powerful Sass tools for writing modular and flexible CSS, but they come with a price. If you allow them to get out of control, they can multiply the amount of CSS created by your Sass very quickly. Practice these guidelines when using <code>@extend</code> :</p>
-
                                 <ul>
                                     <li>Avoid extending classes that have nested rules or complex selectors.</li>
                                     <li>Avoid extending global classes that are likely to be used frequently accross the project.</li>
                                     <li>Use <a href="http://thesassway.com/intermediate/understanding-placeholder-selectors">placeholder classes</a> when possible.</li>
                                 </ul>
 
-
                             </div><!-- chunk -->
 
 
                             <h3>Selector specificity</h3>
                             <p>Use ID selectors extremely sparingly (or not at all) to avoid specificity conflicts. </p>
-
                             <h3>Avoid over-specifying</h3>
                             <p>Strive to limit use of shorthand declarations to instances where you must explicitly set all the available values.</p>
-
                             <pre><code class="scss">// Good
 .thing {
     margin-bottom: 1rem;
@@ -230,28 +221,19 @@ margin: 0px;</code></pre>
     margin: 0 0 1rem 0;
 }</code></pre>
 
-
-
                 </section>
                 <section>
                     <h1 class="page-header">HTML Guidelines</h1>
-
                     <h2>General formatting</h2>
-
                     <ul>
                         <li>Use soft-tabs with a four space indent.</li>
                         <li>Paragraphs of text should always be placed in a <code>&lt;p&gt;</code> tag. Never use multiple &lt;br&gt; tags.</li>
                         <li>Avoid trailing slashes in self-closing elements like <code>&lt;br&gt;</code>, <code>&lt;hr&gt;</code>, <code>&lt;img&gt;</code>, and <code>&lt;input&gt;</code>.</li>
                     </ul>
-
                     <h3>Boolean attributes</h3>
                     <p>Many attributes don't require a value to be set, like disabled or checked, so don't set them.</p>
-
                     <pre><code class="php">&lt;input type="text" disabled&gt;
 &lt;input type="checkbox" value="1" checked&gt;</code></pre>
-
-
-
 
                 </section>
 			</div><!-- column -->
